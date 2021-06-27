cefd924a3706e75ec1132df26a6b6b3bce4a2481
Vera Shamseddine <77043457+verasd9@users.noreply.github.com>
2021-06-27 11:36:02 +0300

    Add files via upload

---
+++ 1test.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.markdown("""<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=9a48fd02-fdd7-4de8-89b9-3f17a9b36179&autoAuth=true&ctid=22d635a3-3930-4779-a82d-155e2d13b75e&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLWVhc3QyLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>""",unsafe_allow_html=True)
